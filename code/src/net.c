9f0f4e2d2fb7ec33f834d17db9e298e8ddfad69f
Symbol <eric5800602@gmail.com>
2022-04-21 20:22:28 +0800

    Add comment and return value of dissect_ip in net.c

--- code/src/net.c
+++ code/src/net.c
@@ -20,22 +20,28 @@ uint16_t cal_ipv4_cksm(struct iphdr iphdr)
 uint8_t *dissect_ip(Net *self, uint8_t *pkt, size_t pkt_len)
 {
     // [TODO]: Collect information from pkt.
-    
+    /* Get Source and Dst IP */
     sprintf(self->src_ip,"%d.%d.%d.%d\n",(unsigned char)(pkt[12]),(unsigned char)(pkt[13]),(unsigned char)(pkt[14]),(unsigned char)(pkt[15]));
     sprintf(self->dst_ip,"%d.%d.%d.%d\n",(unsigned char)(pkt[16]),(unsigned char)(pkt[17]),(unsigned char)(pkt[18]),(unsigned char)(pkt[19]));
     // printf("Soure = %s\n",self->src_ip);
     // printf("Dst = %s\n",self->dst_ip);
+    
     struct ip *iph = (struct ip*) pkt;
+
     //printf("ip_hl = %u\n",(iph->ip_hl)*4);
     self->hdrlen = (size_t)(iph->ip_hl)*4;
+
     /* calculate payload length */
     self->plen = ntohs(iph->ip_len) - self->hdrlen;
     //printf("self->plen = %u\n",self->plen);
+
     /* Check the protocol */
     self->pro = iph->ip_p;
     //printf("0x%x\n",iph->ip_p);
 
     // Return payload of network layer
+    /* Return the pkt + ipv4 header length */
+    return pkt + self->hdrlen;
 }
 
 Net *fmt_net_rep(Net *self)
